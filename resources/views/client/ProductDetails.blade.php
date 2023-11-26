9ab4ceddfd2bbb8f578ed92d4c92b050cdb50c8c
tuana1k56@gmail.com <your.email@example.com>
2023-11-26 20:27:23 +0700

    push

--- resources/views/client/ProductDetails.blade.php
+++ resources/views/client/ProductDetails.blade.php
@@ -57,8 +57,10 @@
                         <div class="product__details__price">{{number_format($value->donGia).' VNĐ'}}</div>
                         <div class="product__details__quantity">
                             <div class="quantity">
-                                <div class="pro-qty">
+                                <div class="pro-qty_card">
+
                                     <input type="number" name="qty" min="0" value="1" />
+
                                     <input type="hidden" name="productid_hidden" value="{{$value->maSanPham}}" />
                                 </div>
                             </div>
